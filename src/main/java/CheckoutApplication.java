66f3f23e47ac276dfcca64d9cad9734e2d4e0781
claudiocostapontes <claudionor.pontes@outlook.com>
2024-09-07 19:43:20 -0300

    commit atualizacao do pom

--- src/main/java/CheckoutApplication.java
+++ src/main/java/CheckoutApplication.java
@@ -1,7 +1,8 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {CouchbaseAutoConfiguration.class})
 public class CheckoutApplication {
     public static void main(String[] args) {
         SpringApplication.run(CheckoutApplication.class, args);
